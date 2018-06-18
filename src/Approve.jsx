8407c8a54ada05556a3c5cbd9e5fa1612cb90be6
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-06-18 04:17:46 +0300

    Update Approve.jsx

--- src/Approve.jsx
+++ src/Approve.jsx
@@ -1,30 +1,87 @@
 import React from 'react';
+import firebase from './firebase.js';
 
 class Approve extends React.Component {
 
-  constructor() {
-    super();
+  state = {
+    isApproved: false
+  }
+
+  constructor(props) {
+    super(props);
 
     this.styles = {
       cellStyle: {
         display: "inline-block"
+      },
+      color6Font:{
+          color: '#d43c61'
       }
+
     }
+
+  }
+
+  componentDidMount() {
+
+    const self = this;
+
+    var database = firebase.database();
+
+    database.ref('approvals/' + this.props.docId + '/' + this.props.userId)
+    .once('value')
+    .then(function (snapshot) {
+
+      console.log('isApproved: ' + snapshot.val());
+
+      self.setState({
+          isApproved: (snapshot.val() != null),
+          dataRetrieved: true
+      });
+
+    });
+
+  }
+
+  handleClick(event) {
+
+    firebase.database()
+    .ref('approvals/' + this.props.docId + '/' + this.props.userId)
+    .push({
+        when: firebase.database.ServerValue.TIMESTAMP
+    });
+
+    this.setState({
+        isApproved: true,
+        dataRetrieved: true
+    });
+
   }
 
   render() {
 
-    return (
-      <div className="col-lg-4 col-md-12" style={this.styles.cellStyle}>
-          <div id="btnApprove">
-              <section className="box">
+    if (this.state.isApproved) {
+        return (
+          <div className="col-lg-4 col-md-12" style={this.styles.cellStyle}>
+              <div id="btnApprove">
+                <section className="box">
+                    <i className="icon big rounded color1 fa-check"></i>
+                    <h3 style={this.styles.color6Font}>אושר</h3>
+                </section>
+            </div>
+            </div>
+            )
+    }  else
+        return (
+          <div className="col-lg-4 col-md-12" style={this.styles.cellStyle}>
+            <div id="btnApprove">
+              <section className="box" onClick={::this.handleClick}>
                   <i className="icon big rounded color1 fa-check"></i>
                   <h3>3. מאשר קריאה</h3>
               </section>
+            </div>
           </div>
-      </div>
-
-    )
+            )
 
   }
 };
